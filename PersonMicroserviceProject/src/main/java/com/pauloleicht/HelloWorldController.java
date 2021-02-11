9d5bfaa43dc0ce33f9f075a0ed2a677e3fb2ab9b
infotecquest <infotecquest@gmail.com>
2021-02-10 21:46:48 -0300

    PersonMicroservice Hello Resource

---
+++ PersonMicroserviceProject/src/main/java/com/pauloleicht/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.pauloleicht;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	private static final String template = "Hello %s.";
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/hello")
+	public HelloWorld say(@RequestParam(value="name", defaultValue = "World") String name) {
+		return new HelloWorld(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+}
